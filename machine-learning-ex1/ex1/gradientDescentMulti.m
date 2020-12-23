33a3ca6e038ee58fc68de1b7a4fc5a6c42e24bd4
Yulypso <khamphousone.thierry@outlook.fr>
2020-12-24 00:21:11 +0100

    added notes

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -6,6 +6,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+% Add intercept term to X with X = [ones(m, 1) X]; ===> see ex1_multi.m
 
 nb_features = size(X, 2);
 

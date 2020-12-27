ba1c93773f100a5885df87837efedd869e0399cd
Yulypso <khamphousone.thierry@outlook.fr>
2020-12-28 00:07:01 +0100

    added plotData logistic regression

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -12,8 +12,17 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+plot(X(find(y==1), 1), X(find(y==1), 2), 'k+', 'LineWidth', 2, 'MarkerSize', 6); % Plot the data of admitted students
+plot(X(find(y==0), 1), X(find(y==0), 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 6); % Plot the data of non admitted students
+ylabel('Exam 2 score'); % Set the y−axis label 
+xlabel('Exam 1 score'); % Set the x−axis label
 
+%k+ => marker en forme de +
+%ko => marker en forme de o
+%MarkerFaceBolor & y permet de definir la couleur des points en jaune ici
 
+%find(y==1) permet de trouver dans nos données toutes les données de classe = 1 => admitted
+%X(data, 1) permet de récuperer uniquement la premiere colonne de la matrice X de taille Mx2 
 
 
 
